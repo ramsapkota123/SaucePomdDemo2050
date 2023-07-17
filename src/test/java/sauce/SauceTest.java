a493afce0605669ddc0dd206022cbe2521c6425c
ramsapkota <rbsapkota99@gmail.com>
2023-07-16 21:25:18 -0500

    helloram

---
+++ src/test/java/sauce/SauceTest.java
@@ -0,0 +1,56 @@
+package sauce;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import sauce.page.SaucePage;
+
+public class SauceTest {
+	private WebDriver driver;
+	private SaucePage saucepage;
+	@BeforeTest
+	public void displaySetUp() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rbsap\\Downloads\\wdriver\\chromedriver.exe");
+	    ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--remote-allow-origins=*");
+	    driver = new ChromeDriver(options);
+	    driver.get("https://www.saucedemo.com/");
+	    driver.manage().window().maximize();
+	    saucepage=new SaucePage(driver);
+	}
+	@Test(priority=1)
+    public void testRightUserNameAndRightPassword() {
+		saucepage.setUserbame("standard_user");
+		saucepage.setPassword("secret_sauce");
+		saucepage.clickButton();
+		
+		
+
+		
+		saucepage.clickBackpack();
+	}
+	@Test(priority=2)	
+   public void clickCartOnSauceDemo() {
+			saucepage.clickCart();
+			
+		}
+	@Test(priority=3)	
+   public void dispalyPaymentGateWay() {
+			
+		
+		
+		saucepage.clickCheckOut();
+		saucepage.FillText("Ram", "Sapkota", "76137");
+		saucepage.displayContinue();
+		saucepage.displayContinue();
+	
+		
+	    	
+	    }
+
+}
---
+++ src/test/java/sauce/page/SaucePage.java
@@ -0,0 +1,94 @@
+package sauce.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SaucePage {
+	private WebDriver driver;
+	
+	@FindBy(id="user-name")
+	WebElement user;
+	
+	@FindBy(id="password")
+	WebElement password;
+	
+
+	@FindBy(id="login-button")
+	WebElement lognbtn;
+	
+
+	@FindBy(id="add-to-cart-sauce-labs-backpack")
+	WebElement backpack;
+	
+
+	@FindBy(xpath="//*[@id=\\\"shopping_cart_container\\\"]/a/span")
+	WebElement cart;
+	
+	@FindBy(id="checkout")
+	WebElement checkout;
+	
+	@FindBy(id="first-name")
+	WebElement name;
+	
+	@FindBy(id="last-name")
+	WebElement lastname;
+	
+	@FindBy(id="postal-code")
+	WebElement postal;
+	
+	@FindBy(id="continue")
+	WebElement contBtn;
+	
+	@FindBy(id="finish")
+	WebElement finishBtn;
+	
+	public SaucePage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	
+	public void setUserbame(String usernameString) {
+		user.sendKeys(usernameString);
+		
+	}
+	public void setPassword(String passwordString) {
+		password.sendKeys(passwordString);
+		
+	}
+	public void clickButton() {
+		lognbtn.click();
+		
+	}
+	public void clickBackpack() {
+		backpack.click();
+		
+	}
+	public void clickCart() {
+		cart.click();
+		
+	}
+	public void clickCheckOut() {
+		checkout.click();
+		
+	}
+	public void FillText(String nameString, String lastnameString, String postalString) {
+		name.sendKeys(nameString);
+		lastname.sendKeys(lastnameString);
+		postal.sendKeys(postalString);
+	}
+		
+   public void displayContinue() {
+	   contBtn.click();
+	   
+   
+		
+	}
+   public void displayFinish() {
+	   finishBtn.click();
+   }
+
+}
